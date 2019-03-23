df8797b13166eecdcbe2f8ffc672e63d0292765a
Adam Kawczyński <a.kawczynski@gmail.com>
2019-03-23 22:10:37 +0100

    New add ad form

--- src/components/ClassifiedForm.vue
+++ src/components/ClassifiedForm.vue
@@ -2,7 +2,42 @@
 <template>
   <div>
     <p>Nowe Ogłoszenie</p>
-    <v-btn round outline @click.native.prevent="postAd()">Zamknij</v-btn>
+    <v-form>
+      <v-layout row wrap>
+        <v-flex md8 sm12 pa-2>
+          <v-text-field
+            v-model="addAdForm.name"
+            label="Tytuł"
+            required
+          ></v-text-field>
+          <v-textarea
+            v-model="addAdForm.desc"
+            label="Opis"
+            required
+          ></v-textarea>
+        </v-flex>
+        <v-flex md4 sm12 pa-2>
+          <v-combobox
+            v-model="addAdForm.categories"
+            :items="addAdForm.categories_set"
+            label="Kategorie"
+            chips
+            clearable
+            solo
+            multiple
+          >
+            <template v-slot:selection="data">
+              <v-chip close @input="removeCat(data.item)">
+                <strong>{{ data.item }}</strong>
+              </v-chip>
+            </template>
+          </v-combobox>
+          <v-text-field clearable solo label="Obrazek"> </v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-form>
+    <v-btn round outline @click.native.prevent="postAd()">Wyślij</v-btn>
+    <v-btn round outline @click.native.prevent="close()">Anuluj</v-btn>
   </div>
 </template>
 
@@ -19,6 +54,8 @@ export default {
         name: "",
         desc: "",
         category: "",
+        categories: [],
+        categories_set: ["susz", "wiecha", "ziarno", "cbd"],
         publisher: "",
         image: "",
         errors: []
@@ -26,16 +63,19 @@ export default {
     };
   },
   methods: {
-    postAd: function(payload) {
-      this.$emit("ad-posted", "lol");
-      /*$backend
-        .postAd(payload)
+    postAd: function() {
+      $backend
+        .postAd(this.addAdForm)
         .then(ad => {
           this.$emit("ad-posted", ad);
         })
         .catch(error => {
           this.error = error;
-        });*/
+        });
+      this.close();
+    },
+    close: function() {
+      this.$emit("ad-posted");
     },
     checkForm: function(e) {
       e.preventDefault();
@@ -50,6 +90,13 @@ export default {
         };
         this.postAd(payload);
       }
+    },
+    removeCat: function(item) {
+      this.addAdForm.categories.splice(
+        this.addAdForm.categories.indexOf(item),
+        1
+      );
+      this.addAdForm.categories = [...this.addAdForm.categories];
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'babel-polyfill'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "babel-polyfill";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(Vuetify, {
-  iconfont: 'mdi', // 'md' || 'mdi' || 'fa' || 'fa4',
+  iconfont: "md", // 'md' || 'mdi' || 'fa' || 'fa4',
   theme: {
-	primary: '#228B22',
+    primary: "#228B22"
   }
-})
+});
 
 new Vue({
-    router,
-    render: h => h(App)
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
